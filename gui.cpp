e3cb200855ae5551e00a975b707b969aa713dec7
supa-hub <71665903+supa-hub@users.noreply.github.com>
2024-05-23 21:35:52 +0300

    Updated the license in gui.cpp

--- gui.cpp
+++ gui.cpp
@@ -1,5 +1,5 @@
 /* 
-Copyright (C) 2024  Tomi Bilcu
+Copyright (C) 2024  Tomi Bilcu a.k.a supa-hub
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
